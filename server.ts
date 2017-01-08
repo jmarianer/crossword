00e04b90d2c0a19f7bddf8f226b79c6071f9969e
Joey Marianer <roie.marianer@gmail.com>
2017-01-08 15:14:42 -0800

    Use the "async" package.

--- server.ts
+++ server.ts
@@ -1,16 +1,17 @@
-// tslint:disable:no-console no-shadowed-variable
+// tslint:disable:no-console
 /// <reference types="node" />
 
 import { createPuzzle } from './create-puzzle';
 import { l10n } from './l10n';
 import { Cell, CellType, ClueDirection, Message, Puzzle } from './types';
 
+import * as async from 'async';
 import * as bodyParser from 'body-parser';
 import * as browserify from 'browserify';
 import * as express from 'express';
 import * as fs from 'fs';
 import * as less from 'less';
-import { Db, MongoClient, ObjectID } from 'mongodb';
+import { Db, MongoCallback, MongoClient, ObjectID } from 'mongodb';
 import * as path from 'path';
 import * as io from 'socket.io';
 import * as tss from 'typescript-simple';
@@ -21,10 +22,16 @@ import createTemplate = require('./templates/create');
 import shareTemplate = require('./templates/share');
 import cluesTemplate = require('./templates/clues');
 
-// tslint:disable:no-var-requires TODO figure out why these can't be imported
+// tslint:disable:no-var-requires TODO write "@types/concat-stream"
 const concat = require('concat-stream');
 
-function serveJs(app: express.Express, url: string, tsFilename: string) {
+// Globals
+let puzzles: {[id: string]: Puzzle} = {};
+let db: Db;
+
+// Helpers for serving Typescript and Less as JS and CSS.
+function serveJs(app: express.Express, url: string, tsFilename: string,
+                 callback: AsyncResultArrayCallback<string, string>) {
   let errors = false;
   console.log('Compiling ' + tsFilename);
   browserify(tsFilename)
@@ -36,42 +43,41 @@ function serveJs(app: express.Express, url: string, tsFilename: string) {
     })
     .pipe(concat((buf: Buffer) => {
       if (errors) {
-        throw 'Error compiling ' + tsFilename;
+        callback('Error compiling ' + tsFilename, null);
       }
       console.log('Finished compiling ' + tsFilename);
-      app.use(url, (req, res, next) => {
+      app.get(url, (req, res) => {
         res.set('Content-Type', 'text/javascript');
         res.send(buf.toString());
       });
+      callback(null, null);
     }));
 }
 
-const app = express();
-app.set('views', __dirname + '/templates');
-serveJs(app, '/js/crossword.js', 'main_ui.ts');
-serveJs(app, '/js/create.js', 'create_ui.ts');
-serveJs(app, '/js/share.js', 'share_ui.ts');
-app.use(bodyParser.urlencoded({ extended: true }));
-fs.readFile('style.less', (err: any, data: Buffer) => {
-  if (err) {
-    throw err;
-  }
-  less.render(data.toString(), (err: any, data: Less.RenderOutput) => {
-    if (err) {
-      throw err;
-    }
-    app.use('/style/style.css', (req, res, next) => {
-      res.set('Content-Type', 'text/css');
-      res.send(data.css);
-    });
-  });
-});
+function serveCss(app: express.Express, url: string, lessFilename: string,
+                  callback: AsyncResultArrayCallback<string, string>) {
+  console.log('Compiling ' + lessFilename);
+  async.waterfall([
+    async.apply(fs.readFile, lessFilename),
+    async.asyncify((data: Buffer) => data.toString()),
+    // TODO: Figure out why this is not the same as just "less.render".
+    (data: string, cb: (error: Less.RenderError, output: Less.RenderOutput) => void) => less.render(data, cb),
+    async.asyncify((data: Less.RenderOutput) => {
+      app.get(url, (req, res) => {
+        res.set('Content-Type', 'text/css');
+        res.send(data.css);
+      });
+      console.log('Finished compiling ' + lessFilename);
+      callback(null, null);
+    }),
+  ]);
+}
 
-let puzzles: {[id: string]: Puzzle} = {};
-
+// Helpers for creating and cloning puzzles.
 function fork(puzzle: Puzzle): Puzzle {
   return JSON.parse(JSON.stringify(puzzle));
 }
+
 function clone(puzzle: Puzzle): Puzzle {
   let newPuzzle = fork(puzzle);
   for (let row of newPuzzle.cells) {
@@ -82,7 +88,7 @@ function clone(puzzle: Puzzle): Puzzle {
   return newPuzzle;
 }
 
-function addPuzzle(db: Db, puzzle: Puzzle, callback: (id: ObjectID) => void) {
+function addPuzzle(puzzle: Puzzle, callback: (id: ObjectID) => void) {
   db.collection('crosswords').insert({ puzzle }, (err, result) => {
     if (err) {
       throw err;
@@ -94,71 +100,80 @@ function addPuzzle(db: Db, puzzle: Puzzle, callback: (id: ObjectID) => void) {
   });
 }
 
-MongoClient.connect(process.env.MONGODB, (err, db) => {
+// Main begins here.
+const app = express();
+
+async.parallel([
+  async.apply(serveJs, app, '/js/crossword.js', 'main_ui.ts'),
+  async.apply(serveJs, app, '/js/create.js', 'create_ui.ts'),
+  async.apply(serveJs, app, '/js/share.js', 'share_ui.ts'),
+  async.apply(serveCss, app, '/style/style.css', 'style.less'),
+  async.apply(async.waterfall, [
+    async.apply(MongoClient.connect, process.env.MONGODB),
+    async.asyncify((tempDb: Db) => { return db = tempDb; }),
+    (unused: Db, cb: MongoCallback<any[]>) => db.collection('crosswords').find().toArray(cb),
+  ]),
+], (err, results) => {
   if (err) {
     throw err;
   }
 
-  db.collection('crosswords').find().toArray((err, data) => {
-    if (err) {
-      throw err;
-    }
+  app.use(bodyParser.urlencoded({ extended: true }));
 
-    for (let i of data) {
-      puzzles[i._id] = i.puzzle;
-    }
+  for (let i of results[4]) {
+    puzzles[i._id] = i.puzzle;
+  }
 
-    app.get('/puzzle/:id', (request, response) => {
-      let puzzid = request.params.id;
-      response.send(puzzleTemplate(puzzid, puzzles[puzzid], l10n[puzzles[puzzid].language]));
-    });
+  app.get('/puzzle/:id', (request, response) => {
+    let puzzid = request.params.id;
+    response.send(puzzleTemplate(puzzid, puzzles[puzzid], l10n[puzzles[puzzid].language]));
+  });
 
-    app.get('/share/:id', (request, response) => {
-      let puzzid = request.params.id;
-      response.send(shareTemplate(puzzid, puzzles[puzzid], l10n[puzzles[puzzid].language]));
-    });
+  app.get('/share/:id', (request, response) => {
+    let puzzid = request.params.id;
+    response.send(shareTemplate(puzzid, puzzles[puzzid], l10n[puzzles[puzzid].language]));
+  });
 
-    app.get('/create/:language', (request, response) => {
-      let language = request.params.language;
-      response.send(createTemplate(language, l10n[language]));
-    });
+  app.get('/create/:language', (request, response) => {
+    let language = request.params.language;
+    response.send(createTemplate(language, l10n[language]));
+  });
 
-    app.post('/getClues', (request, response) => {
-      let newPuzzle = createPuzzle(request.body);
-      let language = request.body.language;
-      response.send(cluesTemplate(language, newPuzzle, l10n[language], request.body.template));
-    });
+  app.post('/getClues', (request, response) => {
+    let newPuzzle = createPuzzle(request.body);
+    let language = request.body.language;
+    response.send(cluesTemplate(language, newPuzzle, l10n[language], request.body.template));
+  });
 
-    app.post('/created', (request, response) => {
-      let newPuzzle = createPuzzle(request.body);
-      addPuzzle(db, newPuzzle, (id) => response.redirect('/puzzle/' + id));
-    });
+  app.post('/created', (request, response) => {
+    let newPuzzle = createPuzzle(request.body);
+    addPuzzle(newPuzzle, (id) => response.redirect('/puzzle/' + id));
+  });
 
-    app.get('/fork/:id', (request, response) => {
-      addPuzzle(db, fork(puzzles[request.params.id]), (id) => response.send(id));
-    });
+  app.get('/fork/:id', (request, response) => {
+    addPuzzle(fork(puzzles[request.params.id]), (id) => response.send(id));
+  });
 
-    app.get('/clone/:id', (request, response) => {
-      addPuzzle(db, clone(puzzles[request.params.id]), (id) => response.send(id));
-    });
+  app.get('/clone/:id', (request, response) => {
+    addPuzzle(clone(puzzles[request.params.id]), (id) => response.send(id));
+  });
 
-    let listener = app.listen(process.env.PORT, () => {
-      console.log('Your app is up');
-    });
+  let listener = app.listen(process.env.PORT, () => {
+    console.log('Your app is up');
+  });
 
-    let ioListener = io(listener);
-    ioListener.on('connection', (socket) => {
-      let puzzid = path.basename(socket.client.request.headers.referer);
-      socket.join(puzzid);
-      socket.on('solution', (msg: Message) => {
-        msg.solution = msg.solution.toUpperCase();
-        if (msg.solution.match(/[םןףץך]/)) {
-          msg.solution = String.fromCharCode(msg.solution.charCodeAt(0) + 1);
-        }
-        puzzles[puzzid].cells[msg.position.row][msg.position.col].solution = msg.solution;
-        db.collection('crosswords').update({_id: new ObjectID(puzzid)}, {$set: {puzzle: puzzles[puzzid]}});
-        ioListener.to(puzzid).emit('solution', msg);
-      });
+  let ioListener = io(listener);
+  ioListener.on('connection', (socket) => {
+    let puzzid = path.basename(socket.client.request.headers.referer);
+    socket.join(puzzid);
+    socket.on('solution', (msg: Message) => {
+      msg.solution = msg.solution.toUpperCase();
+      if (msg.solution.match(/[םןףץך]/)) {
+        msg.solution = String.fromCharCode(msg.solution.charCodeAt(0) + 1);
+      }
+      puzzles[puzzid].cells[msg.position.row][msg.position.col].solution = msg.solution;
+      db.collection('crosswords').update({_id: new ObjectID(puzzid)}, {$set: {puzzle: puzzles[puzzid]}});
+      ioListener.to(puzzid).emit('solution', msg);
     });
   });
 });
